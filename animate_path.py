21818b4d65df7dee9e2fe3634205489048cdb603
SayWhoooaaat <erlend.f@live.com>
2023-10-16 16:58:18 +0200

    Animation frame follows pilot

--- animate_path.py
+++ animate_path.py
@@ -9,8 +9,11 @@ def animate_path(map_metadata, map_image, track_points, fps):
     y0 = map_metadata[2]
     arrow = [(-8,-6), (8,0), (-8,6)]
 
+    height, width = 512, 512
+    center_factor = 0.3
+    center_radius = center_factor / 2 * min(height,width)
+
     # Initialize video writer
-    height, width = map_image.size
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter('animation.mp4', fourcc, fps, (height, width))
 
@@ -27,7 +30,7 @@ def animate_path(map_metadata, map_image, track_points, fps):
 
         # Draws path on image with only path
         if i > 0:
-            draw.line((last_x, last_y, x, y), fill='red')
+            draw.line((last_x, last_y, x, y), fill='red', width=2)
         last_x, last_y = x, y
 
         # Draws arrow at the end of path (but doesnt mess with path_image)
@@ -36,8 +39,18 @@ def animate_path(map_metadata, map_image, track_points, fps):
         angled_arrow = [(x + px * math.cos(phi) - py * math.sin(phi), y + px * math.sin(phi) + py * math.cos(phi)) for px, py in arrow]
         draw2.polygon(angled_arrow, fill='red', outline ='black')
 
+        # Crop out desired frame
+        if i == 0: # center for first iteration
+            xc, yc = x, y
+        center_distance = math.sqrt((x-xc)**2+(y-yc)**2)
+        if center_distance > center_radius: # Move frame center
+            beta = math.atan2(y-yc,x-xc)
+            xc = xc + (center_distance - center_radius) * math.cos(beta)
+            yc = yc + (center_distance - center_radius) * math.sin(beta)
+        cropped_image = image_with_arrow.crop((xc - width/2.0, yc - height/2.0, xc + width/2.0, yc + height/2.0))
+
         # Convert PIL image to NumPy array and write to video
-        frame = np.array(image_with_arrow)
+        frame = np.array(cropped_image)
         out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
     # Release the video writer
